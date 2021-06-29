abd70eece1b1ff305ecfaa9710c32892df71f4e5
Baoyou Xie <baoyou.xie@alibaba-inc.com>
2021-06-29 16:46:14 +0800

    kern: mutex-monitor: 添加对rw-sem的监控

--- SOURCE/module/kernel/mutex.c
+++ SOURCE/module/kernel/mutex.c
@@ -156,6 +156,17 @@ static void (*orig___mutex_lock_slowpath)(atomic_t *lock_count);
 static void (*orig___mutex_unlock_slowpath)(atomic_t *lock_count);
 #endif
 
+static void (*orig_call_rwsem_wake)(struct rw_semaphore *sem);
+static void (*orig_call_rwsem_down_read_failed)(struct rw_semaphore *sem);
+static void (*orig_call_rwsem_down_write_failed)(struct rw_semaphore *sem);
+
+DEFINE_ORIG_FUNC(void, down_read, 1, struct rw_semaphore *, sem);
+DEFINE_ORIG_FUNC(void, up_read, 1, struct rw_semaphore *, sem);
+DEFINE_ORIG_FUNC(void, down_write, 1, struct rw_semaphore *, sem);
+DEFINE_ORIG_FUNC(void, up_write, 1, struct rw_semaphore *, sem);
+DEFINE_ORIG_FUNC(int, down_write_trylock, 1, struct rw_semaphore *, sem);
+DEFINE_ORIG_FUNC(int, down_read_trylock, 1, struct rw_semaphore *, sem);
+
 struct mutex_desc {
 	struct mutex *mutex;
 	u64 lock_time;
@@ -266,7 +277,7 @@ static __used noinline struct mutex_desc *find_desc_alloc(struct mutex *mutex)
 	return desc;
 }
 
-static __used noinline void hook_mutex_lock(struct mutex *lock)
+static __used noinline void hook_lock(void *lock)
 {
 	struct mutex_desc *ret;
 
@@ -276,7 +287,7 @@ static __used noinline void hook_mutex_lock(struct mutex *lock)
 	}
 }
 
-static __used noinline void hook_mutex_unlock(struct mutex *lock)
+static __used noinline void hook_unlock(void *lock)
 {
 	struct mutex_desc *tmp;
 	u64 delay_ns;
@@ -315,7 +326,7 @@ static void diag_mutex_lock(struct mutex *lock)
 
 	if (!__mutex_trylock_fast(lock))
 		orig___mutex_lock_slowpath(lock);
-	hook_mutex_lock(lock);
+	hook_lock(lock);
 }
 #else
 static void diag_mutex_lock(struct mutex *lock)
@@ -327,7 +338,7 @@ static void diag_mutex_lock(struct mutex *lock)
 	 */
 	__mutex_fastpath_lock(&lock->count, *orig___mutex_lock_slowpath);
 	mutex_set_owner(lock);
-	hook_mutex_lock(lock);
+	hook_lock(lock);
 }
 #endif
 
@@ -341,7 +352,7 @@ void new_mutex_lock(struct mutex *lock)
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 18, 0)
 static void diag_mutex_unlock(struct mutex *lock)
 {
-	hook_mutex_unlock(lock);
+	hook_unlock(lock);
 #ifndef CONFIG_DEBUG_LOCK_ALLOC
 	if (__mutex_unlock_fast(lock))
 		return;
@@ -363,7 +374,7 @@ static void diag_mutex_unlock(struct mutex *lock)
 	 */
 	mutex_clear_owner(lock);
 #endif
-	hook_mutex_unlock(lock);
+	hook_unlock(lock);
 	__mutex_fastpath_unlock(&lock->count, *orig___mutex_unlock_slowpath);
 }
 #endif
@@ -373,6 +384,274 @@ void new_mutex_unlock(struct mutex *lock)
 	atomic64_inc_return(&diag_nr_running);
 	diag_mutex_unlock(lock);
 	atomic64_dec_return(&diag_nr_running);
+}
+
+#define RWSEM_READER_OWNED	((struct task_struct *)1UL)
+
+void wrap_call_rwsem_down_read_failed(struct rw_semaphore *sem)
+{
+	orig_call_rwsem_down_read_failed(sem);
+}
+
+void wrap_call_rwsem_wake(struct rw_semaphore *sem)
+{
+	orig_call_rwsem_wake(sem);
+}
+
+void wrap_call_rwsem_down_write_failed(struct rw_semaphore *sem)
+{
+	orig_call_rwsem_down_write_failed(sem);
+}
+
+static inline void diag__down_read(struct rw_semaphore *sem)
+{
+	asm volatile("# beginning down_read\n\t"
+		     LOCK_PREFIX _ASM_INC "(%1)\n\t"
+		     /* adds 0x00000001 */
+		     "  jns        1f\n"
+		     "  call wrap_call_rwsem_down_read_failed\n"
+		     "1:\n\t"
+		     "# ending down_read\n\t"
+		     : "+m" (sem->count)
+		     : "a" (sem)
+		     : "memory", "cc");
+}
+
+/*
+ * unlock after reading
+ */
+static inline void diag__up_read(struct rw_semaphore *sem)
+{
+	long tmp;
+	asm volatile("# beginning __up_read\n\t"
+		     LOCK_PREFIX "  xadd      %1,(%2)\n\t"
+		     /* subtracts 1, returns the old value */
+		     "  jns        1f\n\t"
+		     "  call wrap_call_rwsem_wake\n" /* expects old value in %edx */
+		     "1:\n"
+		     "# ending __up_read\n"
+		     : "+m" (sem->count), "=d" (tmp)
+		     : "a" (sem), "1" (-RWSEM_ACTIVE_READ_BIAS)
+		     : "memory", "cc");
+}
+
+/*
+ * lock for writing
+ */
+#define ____down_write(sem, slow_path)			\
+({							\
+	long tmp;					\
+	struct rw_semaphore* ret;			\
+	register void *__sp asm(_ASM_SP);		\
+							\
+	asm volatile("# beginning down_write\n\t"	\
+		     LOCK_PREFIX "  xadd      %1,(%4)\n\t"	\
+		     /* adds 0xffff0001, returns the old value */ \
+		     "  test " __ASM_SEL(%w1,%k1) "," __ASM_SEL(%w1,%k1) "\n\t" \
+		     /* was the active mask 0 before? */\
+			 "  jz        1f\n"			\
+		     "  call " slow_path "\n"		\
+		     "1:\n"				\
+		     "# ending down_write"		\
+		     : "+m" (sem->count), "=d" (tmp), "=a" (ret), "+r" (__sp) \
+		     : "a" (sem), "1" (RWSEM_ACTIVE_WRITE_BIAS) \
+		     : "memory", "cc");			\
+	ret;						\
+})
+
+static inline void diag___down_write(struct rw_semaphore *sem)
+{
+	____down_write(sem, "wrap_call_rwsem_down_write_failed");
+}
+
+/*
+ * unlock after writing
+ */
+static inline void diag___up_write(struct rw_semaphore *sem)
+{
+	long tmp;
+	asm volatile("# beginning __up_write\n\t"
+		     LOCK_PREFIX "  xadd      %1,(%2)\n\t"
+		     /* subtracts 0xffff0001, returns the old value */
+		     "  jns        1f\n\t"
+			 "  call wrap_call_rwsem_wake\n" /* expects old value in %edx */
+		     "1:\n\t"
+		     "# ending __up_write\n"
+		     : "+m" (sem->count), "=d" (tmp)
+		     : "a" (sem), "1" (-RWSEM_ACTIVE_WRITE_BIAS)
+		     : "memory", "cc");
+}
+
+#ifdef CONFIG_RWSEM_SPIN_ON_OWNER
+/*
+ * All writes to owner are protected by WRITE_ONCE() to make sure that
+ * store tearing can't happen as optimistic spinners may read and use
+ * the owner value concurrently without lock. Read from owner, however,
+ * may not need READ_ONCE() as long as the pointer value is only used
+ * for comparison and isn't being dereferenced.
+ */
+static inline void rwsem_set_owner(struct rw_semaphore *sem)
+{
+	WRITE_ONCE(sem->owner, current);
+}
+
+static inline void rwsem_clear_owner(struct rw_semaphore *sem)
+{
+	WRITE_ONCE(sem->owner, NULL);
+}
+
+static inline void rwsem_set_reader_owned(struct rw_semaphore *sem)
+{
+	/*
+	 * We check the owner value first to make sure that we will only
+	 * do a write to the rwsem cacheline when it is really necessary
+	 * to minimize cacheline contention.
+	 */
+	if (sem->owner != RWSEM_READER_OWNED)
+		WRITE_ONCE(sem->owner, RWSEM_READER_OWNED);
+}
+
+static inline bool rwsem_owner_is_writer(struct task_struct *owner)
+{
+	return owner && owner != RWSEM_READER_OWNED;
+}
+
+static inline bool rwsem_owner_is_reader(struct task_struct *owner)
+{
+	return owner == RWSEM_READER_OWNED;
+}
+#else
+static inline void rwsem_set_owner(struct rw_semaphore *sem)
+{
+}
+
+static inline void rwsem_clear_owner(struct rw_semaphore *sem)
+{
+}
+
+static inline void rwsem_set_reader_owned(struct rw_semaphore *sem)
+{
+}
+#endif
+
+static void diag_down_read(struct rw_semaphore *sem)
+{
+	might_sleep();
+	rwsem_acquire_read(&sem->dep_map, 0, 0, _RET_IP_);
+
+	LOCK_CONTENDED(sem, __down_read_trylock, diag__down_read);
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 9, 0)
+	rwsem_set_reader_owned(sem);
+#endif
+	hook_lock(sem);
+}
+
+static void diag_up_read(struct rw_semaphore *sem)
+{
+	hook_unlock(sem);
+	rwsem_release(&sem->dep_map, 1, _RET_IP_);
+
+	diag__up_read(sem);
+}
+
+static void diag_down_write(struct rw_semaphore *sem)
+{
+	might_sleep();
+	rwsem_acquire(&sem->dep_map, 0, 0, _RET_IP_);
+
+	LOCK_CONTENDED(sem, __down_write_trylock, diag___down_write);
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 9, 0)
+	rwsem_set_owner(sem);
+#endif
+	hook_lock(sem);
+}
+
+/*
+ * trylock for writing -- returns 1 if successful, 0 if contention
+ */
+int diag_down_write_trylock(struct rw_semaphore *sem)
+{
+	int ret = __down_write_trylock(sem);
+
+	if (ret == 1) {
+		rwsem_acquire(&sem->dep_map, 0, 1, _RET_IP_);
+		hook_lock(sem);
+	}
+
+	return ret;
+}
+
+/*
+ * trylock for reading -- returns 1 if successful, 0 if contention
+ */
+int diag_down_read_trylock(struct rw_semaphore *sem)
+{
+	int ret = __down_read_trylock(sem);
+
+	if (ret == 1) {
+		rwsem_acquire_read(&sem->dep_map, 0, 1, _RET_IP_);
+		hook_unlock(sem);
+	}
+	return ret;
+}
+
+static void diag_up_write(struct rw_semaphore *sem)
+{
+	hook_unlock(sem);
+	rwsem_release(&sem->dep_map, 1, _RET_IP_);
+
+	rwsem_clear_owner(sem);
+	diag___up_write(sem);
+}
+
+void new_down_read(struct rw_semaphore *sem)
+{
+	atomic64_inc_return(&diag_nr_running);
+	diag_down_read(sem);
+	atomic64_dec_return(&diag_nr_running);
+}
+
+void new_up_read(struct rw_semaphore *sem)
+{
+	atomic64_inc_return(&diag_nr_running);
+	diag_up_read(sem);
+	atomic64_dec_return(&diag_nr_running);
+}
+
+void new_down_write(struct rw_semaphore *sem)
+{
+	atomic64_inc_return(&diag_nr_running);
+	diag_down_write(sem);
+	atomic64_dec_return(&diag_nr_running);
+}
+
+void new_up_write(struct rw_semaphore *sem)
+{
+	atomic64_inc_return(&diag_nr_running);
+	diag_up_write(sem);
+	atomic64_dec_return(&diag_nr_running);
+}
+
+int new_down_write_trylock(struct rw_semaphore *sem)
+{
+	int ret = 0;
+
+	atomic64_inc_return(&diag_nr_running);
+	ret = diag_down_write_trylock(sem);
+	atomic64_dec_return(&diag_nr_running);
+
+	return ret;
+}
+
+int new_down_read_trylock(struct rw_semaphore *sem)
+{
+	int ret = 0;
+
+	atomic64_inc_return(&diag_nr_running);
+	ret = diag_down_read_trylock(sem);
+	atomic64_dec_return(&diag_nr_running);
+
+	return ret;
 }
 
 #if KERNEL_VERSION(4, 9, 0) <= LINUX_VERSION_CODE
@@ -429,6 +708,12 @@ static int __activate_mutex_monitor(void)
 	new_mutex_lock(orig_text_mutex);
 	JUMP_INSTALL(mutex_lock);
 	JUMP_INSTALL(mutex_unlock);
+	JUMP_INSTALL(down_read);
+	JUMP_INSTALL(up_read);
+	JUMP_INSTALL(down_write);
+	JUMP_INSTALL(up_write);
+	JUMP_INSTALL(down_write_trylock);
+	JUMP_INSTALL(down_read_trylock);
 	new_mutex_unlock(orig_text_mutex);
 	put_online_cpus();
 
@@ -450,6 +735,12 @@ static void __deactivate_mutex_monitor(void)
 	new_mutex_lock(orig_text_mutex);
 	JUMP_REMOVE(mutex_lock);
 	JUMP_REMOVE(mutex_unlock);
+	JUMP_REMOVE(down_read);
+	JUMP_REMOVE(up_read);
+	JUMP_REMOVE(down_write);
+	JUMP_REMOVE(up_write);
+	JUMP_REMOVE(down_write_trylock);
+	JUMP_REMOVE(down_read_trylock);
 	new_mutex_unlock(orig_text_mutex);
 	put_online_cpus();
 
@@ -488,6 +779,7 @@ int mutex_monitor_syscall(struct pt_regs *regs, long id)
 	int ret = 0;
 	struct diag_mutex_monitor_settings settings;
 	static DEFINE_MUTEX(lock);
+	static DECLARE_RWSEM(sem);
 
 	switch (id) {
 	case DIAG_MUTEX_MONITOR_SET:
@@ -539,6 +831,10 @@ int mutex_monitor_syscall(struct pt_regs *regs, long id)
 			for (i = 0; i < ms; i++)
 				mdelay(1);
 			mutex_unlock(&lock);
+			down_write(&sem);
+			for (i = 0; i < ms; i++)
+				mdelay(1);
+			up_write(&sem);
 		}
 		break;
 	default:
@@ -556,6 +852,7 @@ long diag_ioctl_mutex_monitor(unsigned int cmd, unsigned long arg)
 	struct diag_mutex_monitor_settings settings;
 	struct diag_ioctl_dump_param dump_param;
 	static DEFINE_MUTEX(lock);
+	static DECLARE_RWSEM(sem);
 
 	switch (cmd) {
 	case CMD_MUTEX_MONITOR_SET:
@@ -594,6 +891,10 @@ long diag_ioctl_mutex_monitor(unsigned int cmd, unsigned long arg)
 				for (i = 0; i < ms; i++)
 					mdelay(1);
 				mutex_unlock(&lock);
+				down_write(&sem);
+				for (i = 0; i < ms; i++)
+					mdelay(1);
+				up_write(&sem);
 			}
 		}
 		break;
@@ -608,7 +909,17 @@ long diag_ioctl_mutex_monitor(unsigned int cmd, unsigned long arg)
 static int lookup_syms(void)
 {
 	LOOKUP_SYMS(__mutex_lock_slowpath);
-	
+
+	LOOKUP_SYMS(call_rwsem_wake);
+	LOOKUP_SYMS(call_rwsem_down_read_failed);
+	LOOKUP_SYMS(call_rwsem_down_write_failed);
+	LOOKUP_SYMS(down_read);
+	LOOKUP_SYMS(up_read);
+	LOOKUP_SYMS(down_write);
+	LOOKUP_SYMS(up_write);
+	LOOKUP_SYMS(down_write_trylock);
+	LOOKUP_SYMS(down_read_trylock);
+
 	orig___mutex_unlock_slowpath = (void *)diag_kallsyms_lookup_name("__mutex_unlock_slowpath.isra.0");
 	if (orig___mutex_unlock_slowpath == NULL)
 		orig___mutex_unlock_slowpath = (void *)diag_kallsyms_lookup_name("__mutex_unlock_slowpath.isra.12");
@@ -639,6 +950,12 @@ static void jump_init(void)
 {
 	JUMP_INIT(mutex_lock);
 	JUMP_INIT(mutex_unlock);
+	JUMP_INIT(down_read);
+	JUMP_INIT(up_read);
+	JUMP_INIT(down_write);
+	JUMP_INIT(up_write);
+	JUMP_INIT(down_write_trylock);
+	JUMP_INIT(down_read_trylock);
 }
 
 int diag_mutex_init(void)
